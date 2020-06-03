f72785730ecaba7f31be6913b6a94714e37334a1
Peter Kreyenhop <Peter.Kreyenhop@gmail.com>
2020-06-03 23:03:41 +1200

    Pirate talk

--- src/testapp/core.clj
+++ src/testapp/core.clj
@@ -4,7 +4,7 @@
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, PK's Awesome World!"))
+  (println "Hello me hearties arggg, PK's even more awesome World!"))
 
 (defn add2 [x]
   (+ 1 x))
